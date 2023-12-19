e712f672e21d2e883d7a7ce537cab6673f6e1260
bschulz01 <bradley@schulzer.com>
2023-12-19 11:19:50 -0800

    Update December 2023

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'bschulz267@g.ucla.edu';
+  $receiving_email_address = 'byschulz@umich.edu';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
